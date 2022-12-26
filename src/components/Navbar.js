a3bc7be4750a8b486ab1eeb41e1d9b7c12dd9c4b
aman431 <rathod1835@gmail.com>
2022-12-26 21:47:59 +0530

    changes updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -109,7 +109,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/aman431"
                 target="_blank"
                 className="fork-btn-inner"
               >
