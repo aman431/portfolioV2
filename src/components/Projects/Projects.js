7dc54a4bf18143263a194bdc6ccaaccebac1b83b
aman431 <rathod1835@gmail.com>
2022-12-27 14:04:26 +0530

    Added the Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import Sommlike from "../../Assets/Projects/Sommlike.png";
+import Buklup from "../../Assets/Projects/Buklup.png";
+import IJwork from "../../Assets/Projects/IJwork.png";
 
 function Projects() {
   return (
@@ -23,7 +20,18 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={Buklup}
+              isBlog={false}
+              title="Buklup.io  "
+              description={`Buklup is a kind of taxi app. Where users can create a ride. Similar to any other type of taxi app. After the ride created by the rider from the website. It will be shown to the driver app where he can accept that ride and pick the rider from the specific location. Or else we have an admin panel where admin has the authority to assign a ride to the driver from the admin dashboard panel.
+              `}
+              demoLink="https://buklup.sworkstaxi.site/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={IJwork}
               isBlog={false}
               title="IJ-Work"
               description={`IJWork is concept where all the smaller application like cloud, chat, todo and calendar into one application. Support dark mode and arbaic mode as per the client requirment.
@@ -35,57 +43,49 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={Sommlike}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Sommlike"
+              description="Sommlike is the wine collection ECommerce website. It’s a site where all the wine collections are available for the users. Where the users can buy those wines after adding to the add to card. Similar to any ecommerce site. We have also added the sommeliers(sellers) where they can provide their collection of wines to the different customers/users. The online transaction we are using a square."
+              demoLink="https://sommlike.sworks.website/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={
+                "https://user-images.githubusercontent.com/63305945/102266269-371b7280-3f3e-11eb-98bf-414b5748c4d7.png"
+              }
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Web Dev Pool"
+              description="WebDevPool It's a website only for Tech persons(developers community). This web application will be a platform for developers where different developer share their information like education, skills and many more. The application is little similar to LinkedIn but Webdevpool is only related for developer's community.The application was created for recrutiments purpose. So recruiters can visit our application and can visit as many profiles and can select the employee for their company"
+              ghLink="https://github.com/aman431/webdevpool"
+              // demoLink="https://editor.soumya-jit.tech/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={
+                "https://raw.githubusercontent.com/aman431/portfillo/master/Images/Java.png"
+              }
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Hotel Management System"
+              description="Hotel Management is one type of application where Hotel worker can done a billing system of costumer not only billing system but it can also see which rooms are available in hotel through this Desktop Application. I have used a Core Java Language to created this.I have tried to provide different functionality like JDBC connectivity to Database which is start to store a data of different user into local database and many more please look a code for more details."
+              ghLink="https://github.com/aman431/Hotel-Management-Java"
+              // demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={
+                "https://raw.githubusercontent.com/aman431/portfillo/master/Images/Python.png"
+              }
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Ai Jarvis"
+              description="jarvis is one type of AI Application where user can interact to the pc through their voice.. So u just passed a command through your voice and our computer execute that command according to keyword it found."
+              ghLink="https://github.com/aman431/Python-Jarvis"
             />
           </Col>
         </Row>
